dab0b0f6b288b1427e1542a18cd97aa007778018
sivakrishna0266 <57354915+sivakrishna0266@users.noreply.github.com>
2019-11-07 22:34:22 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,25 +1,11 @@
 public class Hello{
 
 	public static void main(String args[]){
-		for (int i=1;i<=10;i++){
+		for (int i=1;i<=2;i++){
 			System.out.println("Hello World....."+i);
 			System.out.println("Thanks for the webhooks demo..");
 			System.out.println("Thanks for the webhooks demo1..");
-			System.out.println("Thanks for the webhooks demonstration..");
-			System.out.println("Thanks for the webhooks demonstration 2..");
-			System.out.println("Thanks for the webhooks demonstration 3..");
-			System.out.println("Thanks for the webhooks demonstration 4..");
-			System.out.println("Thanks for the webhooks demonstration 5..");
-			System.out.println("Thanks for the webhooks demonstration 6..");
-			System.out.println("Thanks for the webhooks demonstration 8..");
-			System.out.println("Thanks for the webhooks demonstration 9..");
-			System.out.println("Thanks for the webhooks demonstration 10..");
-			System.out.println("Thanks for the webhooks demonstration 11..");
-			System.out.println("Thanks for the webhooks demonstration 13..");
-			System.out.println("Thanks for the webhooks demonstration 14..");
-			System.out.println("Thanks for the webhooks demonstration 15..");
-			System.out.println("Thanks for the webhooks demonstration 16..");
-			System.out.println("Thanks for the webhooks demonstration 17..");
+			
 		}
 	}
 	
